076d15ca6ac0ff62a7cbd44a0ef9d3e85618a06f
Suvid Datta <suvid.datta@gmail.com>
2021-02-12 20:41:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,13 @@ def hello_world():
     picture = 'https://www.gravatar.com/avatar/' + md5(b'suvid.datta@gmail.com').hexdigest() + '?s=256'
     return render_template('index.html',ip=ip_address,picture=picture)
 
-
+# THESE ARE EASTER EGGS, DONT TAKE THEM SERIOUSLY
 @app.route('/discord-nitro')
 def nitro():
+    return redirect("https://project-inspirations.herokuapp.com/nevergonnagiveyouup")
+
+@app.route('/nevergonnagiveyouup')
+def lol:
     return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
 
 if __name__ == '__main__':
