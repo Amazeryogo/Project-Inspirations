b827f17d29493bdb4b612e20fd42bc06e8f3532a
Suvid Datta <suvid.datta@gmail.com>
2021-04-03 20:26:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,10 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     ip_address = request.remote_addr
-    picture = 'https://www.gravatar.com/avatar/' + md5(b'suvid.datta@gmail.com').hexdigest() + '?s=256'
+    picture = 'https://www.gravatar.com/avatar/ebab91cbcb3b97466e70e58dcd4cb020?s=256'
     return render_template('index.html',ip=ip_address,picture=picture)
 
-# THESE ARE EASTER EGGS, DONT TAKE THEM SERIOUSLY
-@app.route('/discord-nitro')
-def nitro():
-    return redirect("https://project-inspirations.herokuapp.com/nevergonnagiveyouup")
 
-@app.route('/nevergonnagiveyouup')
-def lol():
-    return redirect("https://ts.lmg.gg/")
 
 if __name__ == '__main__':
     app.run()
