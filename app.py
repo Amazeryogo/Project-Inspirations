9543ac11ecc13b8af58f3715cd1208ee3a7f2f89
Suvid Datta <suvid.datta@gmail.com>
2021-04-25 11:00:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    ip_address = request.remote_addr
+    if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
+        x = request.environ['REMOTE_ADDR']
+    else:
+        x = request.environ['HTTP_X_FORWARDED_FOR']
     picture = 'https://www.gravatar.com/avatar/ebab91cbcb3b97466e70e58dcd4cb020?s=256'
-    return render_template('index.html',ip=ip_address,picture=picture)
+    return render_template('index.html',ip=x,picture=picture)
 
 
 
