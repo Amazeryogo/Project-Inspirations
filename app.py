b9a02361eabbc53135f673dbb91425e599174d5f
Suvid Datta <suvid.datta@gmail.com>
2021-10-06 15:13:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import *
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
     if request.environ.get('HTTP_X_FORWARDED_FOR') is None:
@@ -11,8 +10,6 @@ def hello_world():
         x = request.environ['HTTP_X_FORWARDED_FOR']
     picture = 'https://www.gravatar.com/avatar/ebab91cbcb3b97466e70e58dcd4cb020?s=256'
     return render_template('index.html',ip=x,picture=picture)
-
-
 
 if __name__ == '__main__':
     app.run()
